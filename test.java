ba00d858afe4744b7342ad07d11530bbacab9c0d
liukai0000 <13102127911@163.com>
2017-09-08 22:29:37 +0800

    helloword test output

--- test.java
+++ test.java
@@ -1,8 +1,15 @@
 package com.example.liukai.myapplication;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by liukai on 2017/9/8.
  */
 
-public class test {
+public class test extends Activity {
+    @Override
+    protected void onCreate (Bundle savedInstanceState){
+        System.out.print("this is a test");
+    }
 }
