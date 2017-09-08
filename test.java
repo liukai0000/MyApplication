e329af6192a4bc1bf987d98fce6c601c1c2df522
liukai0000 <13102127911@163.com>
2017-09-08 22:32:25 +0800

    helloword test output delete

--- test.java
+++ test.java
@@ -1,15 +1,8 @@
 package com.example.liukai.myapplication;
 
-import android.app.Activity;
-import android.os.Bundle;
-
 /**
  * Created by liukai on 2017/9/8.
  */
 
-public class test extends Activity {
-    @Override
-    protected void onCreate (Bundle savedInstanceState){
-        System.out.print("this is a test");
-    }
+public class test {
 }
